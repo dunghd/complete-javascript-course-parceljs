a622979a33ef3c5f67a8babc39d55fd512991489
DDH <dung.ho@elca.vn>
2022-05-03 17:20:19 +0700

    possibleChanges

--- src/app.js
+++ src/app.js
@@ -1,104 +1,41 @@
 "use strict";
 
-// function findSum(numbers, queries) {
-//   // Write your code here
-//   const arrReturned = [];
-
-//   for (let i = 0; i < queries.length; i++) {
-//     const startIdx = queries[i][0] - 1; // Index in queries is 1
-//     const endIdx = queries[i][1];
-//     const zeroReplace = queries[i][2];
-
-//     const numbersToSum = numbers.slice(startIdx, endIdx);
-
-//     arrReturned.push(
-//       numbersToSum.reduce((acc, curr) => {
-//         return curr === 0 ? zeroReplace + acc : curr + acc;
-//       }, 0)
-//     );
-//   }
-
-//   return arrReturned;
-// }
-
-function findSum(numbers, queries) {
+function possibleChanges(usernames) {
   // Write your code here
-  const arrReturned = [];
+  const results = [];
 
-  for (let i = 0; i < queries.length; i++) {
-    const startIdx = queries[i][0] - 1; // Index in queries is 1
-    const endIdx = queries[i][1];
-    const zeroReplace = queries[i][2];
+  for (let i = 0; i < usernames.length; i++) {
+    const name = usernames[i];
+    let isCanChange = false;
 
-    let sum = 0;
-
-    for (let j = startIdx; j < endIdx; j++) {
-      if (numbers[j] === 0) {
-        sum += zeroReplace;
+    for (let j = 0; j < name.length - 1; j++) {
+      if (
+        name[j] !== name[j + 1] &&
+        name[j].localeCompare(name[j + 1]) !== -1
+      ) {
+        results.push("YES");
+        isCanChange = true;
+        break;
       } else {
-        sum += numbers[j];
+        continue;
       }
     }
 
-    arrReturned.push(sum);
+    if (!isCanChange) {
+      results.push("NO");
+    }
   }
 
-  return arrReturned;
+  return results;
 }
-
-// // Complete the minimumSwaps function below.
-// function findSum(numbers, queries) {
-//   // Write your code here
-//   const arrReturned = [];
-
-//   let startIdx = 0;
-//   let endIdx = 0;
-
-//   for (let i = 0; i < queries.length; i++) {
-
-//     const zeroReplace = queries[i][2];
-
-//     let sum = 0;
-
-//     if (i === 0) {
-//       for (let j = queries[i][0] - 1; j < queries[i][1]; j++) {
-//         if (numbers[j] === 0) {
-//           sum += zeroReplace;
-//         } else {
-//           sum += numbers[j];
-//         }
-//       }
-//     } else {
-
-//       const startSubIdx = Math.abs(startIdx - (queries[i][0] - 1));
-
-//       for (let j = 0; j < array.length; j++) {
-//         const element = array[j];
-//         for
-//       }
-//     }
-
-//     startIdx = queries[i][0] - 1; // Index in queries is 1
-//     endIdx = queries[i][1];
-
-//     arrReturned.push(sum);
-//   }
-
-//   return arrReturned;
-// }
 
 /*
 
-For each queries i => queries.length:
-- Query i: Sum elements from queries[i][0] to queries[i][1]
--- for j from numbers[queries[i][0]] to numbers[queries[i][1]]
--- store number of zero element
--- Sum + (number-of-zero-element * queries[i][2])
+Take first letter:
+- Compare first letter with each remaining character:
+    if first > letter[i] return YES; break;
+    else continue;
 
 */
 
-const queries = new Array(1)
-  .fill(1)
-  .map(() => new Array(3).fill(1, 0, 1).fill(3, 1, 2).fill(10, 2, 3));
-
-console.log(findSum([20, 30, 0, 10], queries));
+console.log(possibleChanges(["bee", "superhero", "aec"]));
