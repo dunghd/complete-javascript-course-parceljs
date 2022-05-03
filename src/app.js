6b200102f91b2b13d6376642848e72d290099b08
DDH <dung.ho@elca.vn>
2022-05-03 17:19:16 +0700

    findSum of sub array

--- src/app.js
+++ src/app.js
@@ -1,58 +1,104 @@
 "use strict";
 
-function minimumBribes(q) {
+// function findSum(numbers, queries) {
+//   // Write your code here
+//   const arrReturned = [];
+
+//   for (let i = 0; i < queries.length; i++) {
+//     const startIdx = queries[i][0] - 1; // Index in queries is 1
+//     const endIdx = queries[i][1];
+//     const zeroReplace = queries[i][2];
+
+//     const numbersToSum = numbers.slice(startIdx, endIdx);
+
+//     arrReturned.push(
+//       numbersToSum.reduce((acc, curr) => {
+//         return curr === 0 ? zeroReplace + acc : curr + acc;
+//       }, 0)
+//     );
+//   }
+
+//   return arrReturned;
+// }
+
+function findSum(numbers, queries) {
   // Write your code here
-  let nbBribe = 0;
-  let personBribed = {};
-  let tooChaotic = false;
+  const arrReturned = [];
 
-  for (let i = q.length - 1; i > 0; i--) {
-    let noSwaps = true;
-    for (let j = 0; j < i; j++) {
-      if (q[j] > q[j + 1]) {
-        personBribed[q[j]] = (personBribed[q[j]] || 0) + 1;
+  for (let i = 0; i < queries.length; i++) {
+    const startIdx = queries[i][0] - 1; // Index in queries is 1
+    const endIdx = queries[i][1];
+    const zeroReplace = queries[i][2];
 
-        if (personBribed[q[j]] > 2) {
-          tooChaotic = true;
-          console.log(`Too chaotic`);
-          break;
-        }
+    let sum = 0;
 
-        nbBribe++;
-        [q[j], q[j + 1]] = [q[j + 1], q[j]];
-        noSwaps = false;
+    for (let j = startIdx; j < endIdx; j++) {
+      if (numbers[j] === 0) {
+        sum += zeroReplace;
+      } else {
+        sum += numbers[j];
       }
     }
 
-    if (tooChaotic || noSwaps) {
-      break;
-    }
+    arrReturned.push(sum);
   }
 
-  if (!tooChaotic) {
-    console.log(nbBribe);
-  }
+  return arrReturned;
 }
 
+// // Complete the minimumSwaps function below.
+// function findSum(numbers, queries) {
+//   // Write your code here
+//   const arrReturned = [];
+
+//   let startIdx = 0;
+//   let endIdx = 0;
+
+//   for (let i = 0; i < queries.length; i++) {
+
+//     const zeroReplace = queries[i][2];
+
+//     let sum = 0;
+
+//     if (i === 0) {
+//       for (let j = queries[i][0] - 1; j < queries[i][1]; j++) {
+//         if (numbers[j] === 0) {
+//           sum += zeroReplace;
+//         } else {
+//           sum += numbers[j];
+//         }
+//       }
+//     } else {
+
+//       const startSubIdx = Math.abs(startIdx - (queries[i][0] - 1));
+
+//       for (let j = 0; j < array.length; j++) {
+//         const element = array[j];
+//         for
+//       }
+//     }
+
+//     startIdx = queries[i][0] - 1; // Index in queries is 1
+//     endIdx = queries[i][1];
+
+//     arrReturned.push(sum);
+//   }
+
+//   return arrReturned;
+// }
+
 /*
-  Similar Bubble sort?
 
-  [2, 5, 1, 3, 4]
-
-   i =>  
-
-  i = 0: 2 < 5 ? true i++
-  i = 1: 5 < 1 ? false => replace(5, 1) i++
-
-  [2, 1, 5, 3, 4] => 3
-  [2, 5, 1, 3, 4] => Too chaotic
-  5 > 1 swap
-  5 > 3 swap
-  5 > 4 swap
+For each queries i => queries.length:
+- Query i: Sum elements from queries[i][0] to queries[i][1]
+-- for j from numbers[queries[i][0]] to numbers[queries[i][1]]
+-- store number of zero element
+-- Sum + (number-of-zero-element * queries[i][2])
 
 */
 
-// console.log(minimumBribes([1, 2, 5, 3, 7, 8, 6, 4]));
-// console.log(minimumBribes([1, 2, 3, 5, 4, 6, 7, 8]));
-// console.log(minimumBribes([2, 1, 5, 3, 4]));
-console.log(minimumBribes([2, 5, 1, 3, 4]));
+const queries = new Array(1)
+  .fill(1)
+  .map(() => new Array(3).fill(1, 0, 1).fill(3, 1, 2).fill(10, 2, 3));
+
+console.log(findSum([20, 30, 0, 10], queries));
